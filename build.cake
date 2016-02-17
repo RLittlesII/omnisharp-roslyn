e19b931a4ba335bda48d72bb334d6e07b6771c85
Rodney Littles II <rodney.littles@gmail.com>
2016-02-16 21:06:20 -0600

    Cake build unit test runner

---
+++ build.cake
@@ -0,0 +1,197 @@
+// ADDINS
+
+// TOOLS
+#tool "xunit.runner.console"
+
+// ARGUMENTS
+var target = Argument("target", "Default");
+var configuration = Argument("configuration","Release");
+
+// ENVIRONMENT
+var isAppVeyorBuild = AppVeyor.IsRunningOnAppVeyor;
+var isUnix = Machine.IsUnix();
+var buildEnvironment = EnvironmentVariable("TRAVIS_OS_NAME");
+var homeDirectory = EnvironmentVariable("HOME");
+var dnxRuntime = "1.0.0-rc2-16444";
+var workingDirectory = System.IO.Directory.GetCurrentDirectory();
+
+// VARIABLES
+var artifactsFolder = Directory("../artifacts");
+var targetFrameworks = new[] { "dnx451", "dnxcore50" };
+var configurations = new[] { target };
+var solution = "../src/*";
+// var solution = File("../omnisharp.json");
+
+// FUNCTIONS
+Action<string> SetRuntime = (runtime) =>
+{
+    var processSettings = new ProcessSettings
+    {
+        Arguments = new ProcessArgumentBuilder().Append("use {0} -r {1}", dnxRuntime, runtime)
+    };
+    using(var process = StartAndReturnProcess("dnvm.sh", processSettings))
+    {
+        process.WaitForExit();
+        Information("dnvm setup complete.");
+    }
+};
+
+Action<string, string> Test = (project, runtime) =>
+{
+    //Information(MakeAbsolute(Directory(project)));
+    var processSettings = new ProcessSettings
+    {
+        Arguments = new ProcessArgumentBuilder().Append(string.Format("-p {0} test -parallel none", MakeAbsolute(Directory(project))))
+    };
+    
+    SetRuntime(runtime);
+    
+    Information("Setting working directory: {0}", MakeAbsolute(Directory(project)));
+    System.IO.Directory.SetCurrentDirectory(project);
+    
+    Information("Running tests on project: {0}", project);
+    using(var process = StartAndReturnProcess("dnx", processSettings))
+    {
+        process.WaitForExit();
+        Information("dnx tests setup complete.");
+    }
+};
+
+Action<string, string, string, string, string> Publish = (project, version, runtime, destination, tar) => 
+{
+    // Set the dnvm version and runtime
+    
+    // run the dnu publish --configuration Release --no-source --quiet --runtime active --out $_dest
+    var dnuBuildSettings = new DNUBuildSettings
+    {
+        Frameworks = new[] { runtime },
+        Configurations = new[] { target },
+        OutputDirectory = destination,
+        Quiet = true
+    };
+    
+    DNUBuild(project, dnuBuildSettings);
+    
+    //TODO: Zip Files
+};
+
+
+Action<string> Pack = (project) =>
+{
+    var dnuPackSettings = new DNUPackSettings
+    {
+        Frameworks = targetFrameworks,
+        Configurations = configurations,
+        OutputDirectory = string.Format("{0}/nuget", artifactsFolder),
+        Quiet = true
+    };
+    
+    Information("DNU Restore: {0}", project);
+    DNURestore(project);
+    
+    Information("DNU Pack: {0}", project);
+    DNUPack(project, dnuPackSettings);
+};
+
+
+
+// TASKS
+
+Task("Environment")
+    .Does(() =>
+    {
+        // Check for Omnisharp environment
+        // build array of needed variables
+        // Install Omnisharp environment
+        // OMNISHARP_VERSION
+        Information("Setting Environment Variable");
+        
+        // SetEnvironmentVariable("TESTCAKE", "SUCCESS!!!", EnvironmentVariableTarget.Machine);
+        // var env = EnvironmentVariable("TESTCAKE");
+        // Information(env);
+    });
+
+Task("Download")
+    .Does(() =>
+    {
+        // Download Dependencies
+        // dnvm
+        // xunit runner
+        
+        Information("Downloading the dnvm installation script");
+        if(isUnix)
+        {
+            DownloadFile("https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh", "./dnvminstall.sh");
+        }
+    });
+
+Task("Install")
+    .IsDependentOn("Download")
+    .Does(() => 
+    {
+        // Install the dnx
+        StartAndReturnProcess("./dnvminstall.sh");
+        
+        // Install the dnu
+        
+        // Install mono run time
+    });
+    
+Task("Clean")
+    //.IsDependentOn("Install")
+    .IsDependentOn("Download")
+    .Does(() => 
+    {
+        // CleanDirectories(artifactsFolder);
+    });
+    
+Task("Restore")
+    //.IsDependentOn("Install")
+    .IsDependentOn("Download")
+    .IsDependentOn("Clean")
+    .Does(() =>
+    {
+        NuGetRestore(solution);
+    });
+
+Task("Test")
+    //.IsDependentOn("Download")
+    //.IsDependentOn("Install")
+    //.IsDependentOn("Clean")
+    //.IsDependentOn("Restore")
+    .Does(() =>
+    {   Information("This is only a test");
+        Test("./tests/OmniSharp.Bootstrap.Tests", "coreclr");
+    });
+    
+Task("Build")
+    .Does(() =>
+    {
+        
+    });
+
+Task("Patch")
+    .Does(() => 
+    {
+    });
+
+Task("Package")
+    .IsDependentOn("Test")
+    .Does(() => 
+    {
+    });
+
+Task("Publish")
+    .IsDependentOn("Test")
+    .Does(() => 
+    {
+    });
+
+Task("Default")
+    .IsDependentOn("Build")
+    .Does(() => 
+    {
+        
+    });
+    
+RunTarget(target);
--- build.sh
+++ build.sh
@@ -1,156 +1,83 @@
 #!/bin/bash
-_test() {
-  local _project="$1"
-  local _runtime="$2"
-  dnvm use 1.0.0-rc2-16444 -r $_runtime
-  pushd tests/$_project
-  dnx test -parallel none
-  rc=$?; if [[ $rc != 0 ]]; then
-    echo "Tests failed for tests/$_project with runtime $_runtime"
-    exit $rc;
-  fi
-  popd
-}
+###############################################################
+# This is the Cake bootstrapper script that is responsible for
+# downloading Cake and all specified tools from NuGet.
+###############################################################
 
-_patch_project() {
-  local _project="$1"
-  jq '.version="'$OMNISHARP_VERSION'"' src/$_project/project.json > src/$_project/project.json.temp
-  mv src/$_project/project.json.temp src/$_project/project.json
-}
+# Define directories.
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TOOLS_DIR=$SCRIPT_DIR/tools
+NUGET_EXE=$TOOLS_DIR/nuget.exe
+CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
 
-_pack() {
-  local _project="$1"
-  dnu restore src/$_project --quiet
-  dnu pack src/$_project --configuration Release --out artifacts/nuget --quiet
-  rc=$?; if [[ $rc != 0 ]]; then
-    echo "Pack failed for src/$_project"
-    exit 1;
-  fi
-}
+# Define default arguments.
+SCRIPT="build.cake"
+TARGET="Default"
+CONFIGURATION="Release"
+VERBOSITY="minimal"
+DRYRUN=false
+SHOW_VERSION=false
 
-_publish() {
-  local _project="$1"
-  local _runtime="$2"
-  local _version="1.0.0-rc2-16444"
-  local _dest="$3"
-  local _tar="$4"
+# Parse arguments.
+for i in "$@"; do
+    case $1 in
+        -s|--script) SCRIPT="$2"; shift ;;
+        -t|--target) TARGET="$2"; shift ;;
+        -c|--configuration) CONFIGURATION="$2"; shift ;;
+        -v|--verbosity) VERBOSITY="$2"; shift ;;
+        -d|--dryrun) DRYRUN=true ;;
+        --version) SHOW_VERSION=true ;;
+    esac
+    shift
+done
 
-  dnvm use $_version -r $_runtime
-  dnu publish src/$_project --configuration Release --no-source --quiet --runtime active --out $_dest
-  rc=$?; if [[ $rc != 0 ]]; then
-    echo "Publish failed for src/$_project with runtime $_runtime, destination: $_dest"
-    exit 1;
-  fi
-
-  pushd $_dest/approot/packages/$_project/1.0.0/root/
-  jq '.entryPoint="OmniSharp.Host"' project.json > project.json.temp
-  mv project.json.temp project.json
-  popd
-
-  tree -if $_dest | grep .nupkg | xargs rm
-  pushd $_dest/approot
-  tar -zcf "../$_tar.tar.gz" .
-  rc=$?; if [[ $rc != 0 ]]; then
-    echo "Tar failed for src/$_project with runtime $_runtime, destination: $_dest"
-    exit 1;
-  fi
-  popd
-}
-#########################
-if (! $TRAVIS) then
-    pushd "$(dirname "$0")"
+# Make sure the tools folder exist.
+if [ ! -d "$TOOLS_DIR" ]; then
+  mkdir "$TOOLS_DIR"
 fi
 
-rm -rf artifacts
-if ! type dnvm > /dev/null 2>&1; then
-    curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+# Make sure that packages.config exist.
+if [ ! -f "$TOOLS_DIR/packages.config" ]; then
+    echo "Downloading packages.config..."
+    curl -Lsfo "$TOOLS_DIR/packages.config" http://cakebuild.net/bootstrapper/packages
+    if [ $? -ne 0 ]; then
+        echo "An error occured while downloading packages.config."
+        exit 1
+    fi
 fi
 
-# Handle to many files on osx
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  ulimit -n 4096
+# Download NuGet if it does not exist.
+if [ ! -f "$NUGET_EXE" ]; then
+    echo "Downloading NuGet..."
+    curl -Lsfo "$NUGET_EXE" https://www.nuget.org/nuget.exe
+    if [ $? -ne 0 ]; then
+        echo "An error occured while downloading nuget.exe."
+        exit 1
+    fi
 fi
 
-# work around restore timeouts on Mono
-[ -z "$MONO_THREADS_PER_CPU" ] && export MONO_THREADS_PER_CPU=50
+# Restore tools from NuGet.
+pushd "$TOOLS_DIR" >/dev/null
+mono "$NUGET_EXE" install -ExcludeVersion
+if [ $? -ne 0 ]; then
+    echo "Could not restore NuGet packages."
+    exit 1
+fi
+popd >/dev/null
 
-export DNX_UNSTABLE_FEED=https://www.myget.org/F/aspnetcidev/api/v2
-dnvm update-self
-
-dnvm install 1.0.0-rc2-16444 -u -r mono
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-dnvm install 1.0.0-rc2-16444 -u -r coreclr
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-dnu restore --quiet --parallel
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-_test "OmniSharp.Bootstrap.Tests" "coreclr"
-_test "OmniSharp.Bootstrap.Tests" "mono"
-_test "OmniSharp.Dnx.Tests" "coreclr"
-_test "OmniSharp.Dnx.Tests" "mono"
-#_test "OmniSharp.MSBuild.Tests" "coreclr"
-_test "OmniSharp.MSBuild.Tests" "mono"
-_test "OmniSharp.Plugins.Tests" "coreclr"
-_test "OmniSharp.Plugins.Tests" "mono"
-_test "OmniSharp.Roslyn.CSharp.Tests" "coreclr"
-_test "OmniSharp.Roslyn.CSharp.Tests" "mono"
-#_test "OmniSharp.ScriptCs.Tests" "coreclr"
-_test "OmniSharp.ScriptCs.Tests" "mono"
-_test "OmniSharp.Stdio.Tests" "coreclr"
-_test "OmniSharp.Stdio.Tests" "mono"
-_test "OmniSharp.Tests" "coreclr"
-_test "OmniSharp.Tests" "mono"
-
-OMNISHARP_VERSION="1.0.0-dev";
-if [ $TRAVIS_TAG ]; then
-  OMNISHARP_VERSION=${TRAVIS_TAG:1};
+# Make sure that Cake has been installed.
+if [ ! -f "$CAKE_EXE" ]; then
+    echo "Could not find Cake.exe at '$CAKE_EXE'."
+    exit 1
 fi
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  # omnisharp-coreclr-darwin-x64.tar.gz
-  _publish "OmniSharp" "coreclr" "artifacts/omnisharp-coreclr" "../omnisharp-coreclr-darwin-x64"
-  # omnisharp.bootstrap-coreclr-darwin-x64.tar.gz
-  _publish "OmniSharp.Bootstrap" "coreclr" "artifacts/omnisharp.bootstrap-coreclr" "../omnisharp.bootstrap-coreclr-darwin-x64"
+# Start Cake
+if $SHOW_VERSION; then
+    mono "$CAKE_EXE" -version
+elif $DRYRUN; then
+    mono "$CAKE_EXE" $SCRIPT -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET -dryrun
 else
-  # omnisharp-coreclr-linux-x64.tar.gz
-  _publish "OmniSharp" "coreclr" "artifacts/omnisharp-coreclr" "../omnisharp-coreclr-linux-x64"
-  # omnisharp-mono.tar.gz
-  _publish "OmniSharp" "mono" "artifacts/omnisharp-coreclr" "../omnisharp-mono"
-
-  # omnisharp-coreclr-linux-x64.tar.gz
-  _publish "OmniSharp.Bootstrap" "coreclr" "artifacts/omnisharp.bootstrap-coreclr" "../omnisharp.bootstrap-coreclr-linux-x64"
-  # omnisharp-mono.tar.gz
-  _publish "OmniSharp.Bootstrap" "mono" "artifacts/omnisharp.bootstrap-coreclr" "../omnisharp.bootstrap-mono"
-
-  if [ $TRAVIS ]; then
-    _patch_project "OmniSharp.Host"
-    _patch_project "OmniSharp.Abstractions"
-    _patch_project "OmniSharp.Bootstrap"
-    _patch_project "OmniSharp.Dnx"
-    _patch_project "OmniSharp.MSBuild"
-    _patch_project "OmniSharp.Nuget"
-    _patch_project "OmniSharp.Roslyn"
-    _patch_project "OmniSharp.Roslyn.CSharp"
-    _patch_project "OmniSharp.ScriptCs"
-    _patch_project "OmniSharp.Stdio"
-  fi
-
-  _pack "OmniSharp.Host"
-  _pack "OmniSharp.Abstractions"
-  _pack "OmniSharp.Bootstrap"
-  _pack "OmniSharp.Dnx"
-  _pack "OmniSharp.MSBuild"
-  _pack "OmniSharp.Nuget"
-  _pack "OmniSharp.Roslyn"
-  _pack "OmniSharp.Roslyn.CSharp"
-  _pack "OmniSharp.ScriptCs"
-  _pack "OmniSharp.Stdio"
+    mono "$CAKE_EXE" $SCRIPT -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET
 fi
 
-tree artifacts
-
-if (! $TRAVIS) then
-    popd
-fi
+exit $?
