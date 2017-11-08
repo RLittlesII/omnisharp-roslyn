777a288d2e3c03bc03ffe60f85585ef2433ff543
RLittlesII <rodney.littles@gmail.com>
2017-11-07 22:42:07 -0600

    Adds CodeLens

---
+++ src/OmniSharp.LanguageServerProtocol/Handlers/CodeLensHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OmniSharp.Extensions.LanguageServer.Capabilities.Client;
+using OmniSharp.Extensions.LanguageServer.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Mef;
+using OmniSharp.Models.CodeAction;
+
+namespace OmniSharp.LanguageServerProtocol.Handlers
+{
+    public class CodeLensHandler : ICodeLensHandler
+    {
+        private readonly IRequestHandler<RunCodeActionRequest, RunCodeActionResponse> _runCodeActionHandler;
+        private readonly IRequestHandler<GetCodeActionRequest, GetCodeActionsResponse> _getCodeActionHandler;
+        private readonly OmniSharpWorkspace _workspace;
+
+        public CodeLensHandler(Mef.IRequestHandler<RunCodeActionRequest, RunCodeActionResponse> runCodeActionHandler, IRequestHandler<GetCodeActionRequest, GetCodeActionsResponse> getCodeActionHandler,
+            OmniSharpWorkspace workspace)
+        {
+            _runCodeActionHandler = runCodeActionHandler;
+            _getCodeActionHandler = getCodeActionHandler;
+            _workspace = workspace;
+        }
+
+        public async Task<CodeLensContainer> Handle(CodeLensParams request, CancellationToken token)
+        {
+            var getCodeActionRequest = new GetCodeActionRequest
+            {
+            };
+
+            var omnisharpResponse = await _getCodeActionHandler.Handle(getCodeActionRequest);
+            return new CodeLensContainer();
+        }
+
+        public CodeLensRegistrationOptions GetRegistrationOptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCapability(CodeLensCapability capability)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OmniSharp.LanguageServerProtocol/Handlers/CodeLensResolverHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OmniSharp.Extensions.JsonRpc;
+using OmniSharp.Extensions.LanguageServer.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Models;
+using OmniSharp.Models.CodeAction;
+using OmniSharp.Models.FindUsages;
+
+namespace OmniSharp.LanguageServerProtocol.Handlers
+{
+    public class CodeLensResolverHandler : ICodeLensResolveHandler
+    {
+        private readonly IRequestHandler<FindUsagesRequest, QuickFixResponse> _findUsageRequestHandler;
+
+        public CodeLensResolverHandler(IRequestHandler<FindUsagesRequest, QuickFixResponse> findUsageRequestHandler)
+        {
+            _findUsageRequestHandler = findUsageRequestHandler;
+        }
+
+        public async Task<CodeLens> Handle(CodeLens request, CancellationToken token)
+        {
+            var omnisharpRequest = new FindUsagesRequest
+            {
+            };
+
+            var omnisharpResponse = await _findUsageRequestHandler.Handle(omnisharpRequest, token);
+
+            var lens = new CodeLens
+            {
+            };
+
+            return lens;
+        }
+    }
+}
